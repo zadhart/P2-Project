0070d87597db7f93aad5f92d079b756b743d212f
zadhart <wams@ic.ufal.br>
2021-06-28 21:15:56 -0300

    Database created

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projetoP2.sqlite3'
+db = SQLAlchemy(app)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Empregado(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    endereco = db.Column(db.String(255))
+    tipo = db.Column(db.String(255), nullable=False)
+    tipoPagamento = db.Column(db.String(255), nullable=False)
+    agendaPagamento = db.Column(db.String(255), nullable=False)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class Assalariados(db.Model):
+    id = db.Column(db.Integer, db.ForeignKey("empregado.id"), primary_key=True, unique=True, nullable=False)
+    salario = db.Column(db.Float)
+
+
+class Horistas(db.Model):
+    id = db.Column(db.Integer, db.ForeignKey("empregado.id"), primary_key=True, unique=True, nullable=False)
+    horasTrabalhadas = db.Column(db.Float)
+
+
+class Comissionados(db.Model):
+    id = db.Column(db.Integer, db.ForeignKey("empregado.id"), primary_key=True, unique=True, nullable=False)
+    salario = db.Column(db.Float, nullable=False)
+    comissaoP = db.Column(db.Float, nullable=False)
+
+
+class Vendas(db.Model):
+    vid = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    id = db.Column(db.Integer, db.ForeignKey("empregado.id"), primary_key=True, unique=True, nullable=False)
+    valor = db.Column(db.Float)
+    date = db.Column(db.DateTime, nullable=False)
+
+
+class PagSemanal(db.Model):
+    id = db.Column(db.Integer, db.ForeignKey("empregado.id"), primary_key=True, unique=True, nullable=False)
+    diaSem = db.Column(db.String, nullable=False)
+
+
+class PagBiSemanal(db.Model):
+    id = db.Column(db.Integer, db.ForeignKey("empregado.id"), primary_key=True, unique=True, nullable=False)
+    diaMes = db.Column(db.Integer, nullable=False)
+
+
+class PagMensal(db.Model):
+    id = db.Column(db.Integer, db.ForeignKey("empregado.id"), primary_key=True, unique=True, nullable=False)
+    diaMes = db.Column(db.Integer, nullable=False)
+
+
+class Sindicato(db.Model):
+    sid = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    id = db.Column(db.Integer, db.ForeignKey("empregado.id"), unique=True, nullable=False)
+    taxa = db.Column(db.Float)
+    taxaAdic = db.Column(db.Float)
+
+
+db.create_all()
