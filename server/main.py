0b2b69df86bfa69cad35071d3461de2ee9156786
zadhart <wams@ic.ufal.br>
2021-08-10 21:23:28 -0300

    Refatorando o código

--- server/main.py
+++ server/main.py
@@ -1,173 +1,16 @@
-from datetime import datetime
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-import uuid
+from flask import request
+from create_app import db, app
+from Change import Change
+from Empregado import Empregado
+from Pagamentos import Pagamentos
+from Pontos import Pontos
+from Vendas import Vendas
+from Sindicato import Sindicato
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projetoP2.sqlite3'
-db = SQLAlchemy(app)
 last_changes = []
 
 
-class Change():
-    def __init__(self, new_data, prev_data, change_function, undo_function):
-        self.prev_data = prev_data
-        self.new_data = new_data
-        self.change_function = change_function
-        self.undo_function = undo_function
-
-    def rmv_employee(self):
-        '''content = request.get_json()
-
-        obj = db.session.query(Empregado).filter(Empregado.id == content["id"]).first()'''
-        obj = self.new_data
-        db.session.delete(obj)
-        db.session.commit()
-
-        return "Employee removed"
-
-    def add_employee(self):
-        db.session.add(self.prev_data)
-        db.session.commit()
-
-    def undoUpdate(self):
-        self.rmv_employee()
-        self.add_employee()
-        db.session.commit()
-
-        print("updated")
-        return "Updated"
-
-    def cancelSell(self):
-        '''content = request.get_json()
-
-        obj = db.session.query(Vendas).filter(Vendas.vid == content["vid"]).first()'''
-        obj = self.prev_data
-        db.session.delete(obj)
-        db.session.commit()
-
-        return "Sell canceled"
-
-    def unreadTcard(self):
-        '''content = request.get_json()
-
-        obj = db.session.query(Pontos).filter(Pontos.pid == content["pid"]).first()'''
-        obj = self.prev_data
-        db.session.delete(obj)
-        db.session.commit()
-
-        return "Unread Tcard"
-
-    def undo(self):
-        if self.undo_function == "rmv_employee":
-            self.rmv_employee()
-
-        elif self.undo_function == "cancel_sell":
-            self.cancelSell()
-
-        elif self.undo_function == "unread_tcard":
-            self.unreadTcard()
-
-        else:
-            self.undoUpdate()
-
-
-
-# Creating the ORM objects
-class Empregado(db.Model):
-    id = db.Column(db.String(255), unique=True, primary_key=True)
-    nome = db.Column(db.String(255), nullable=False)
-    endereco = db.Column(db.String(255))
-
-    # Criando os relacionamentos entre as tabelas
-    pontos = db.relationship("Pontos", backref="empregado", uselist=False, cascade="all,delete")
-    vendas = db.relationship("Vendas", backref="empregado", uselist=False, cascade="all,delete")
-    sindicato = db.relationship("Sindicato", backref="empregado", uselist=False, cascade="all,delete")
-    pagamento = db.relationship("Pagamentos", backref="empregado", uselist=False, cascade="all,delete")
-
-    def __init__(self, nome, endereco):
-        self.id = str(uuid.uuid4())
-        self.nome = nome
-        self.endereco = endereco
-
-
-class Pagamentos(db.Model):
-    pid = db.Column(db.String(255), primary_key=True, unique=True, nullable=False)
-    id = db.Column(db.String(255), db.ForeignKey("empregado.id", ondelete='CASCADE'))
-    tipo = db.Column(db.String(255), nullable=False)
-    diaMes = db.Column(db.Integer)
-    diaSem = db.Column(db.String(255))
-    tipoSem = db.Column(db.String(255))
-    salario = db.Column(db.Float)
-    comissao = db.Column(db.Float)
-    salarioHora = db.Column(db.Float)
-
-    def __init__(self, id, tipo, diaMes, diaSem, tipoSem, salario, comissao, salarioHora):
-        self.pid = str(uuid.uuid4())
-        self.id = id
-        self.tipo = tipo
-        self.diaMes = diaMes
-        self.diaSem = diaSem
-        self.tipoSem = tipoSem
-        self.salario = salario
-        self.salarioHora = salarioHora
-        self.comissao = comissao
-
-
-class Pontos(db.Model):
-    pid = db.Column(db.String(255), primary_key=True, unique=True, nullable=False)
-    id = db.Column(db.String(255), db.ForeignKey("empregado.id", ondelete='CASCADE'))
-    horasTrabalhadas = db.Column(db.Float)
-    mes = db.Column(db.Integer, nullable=False)
-    semana = db.Column(db.Integer, nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
-
-    def __init__(self, id, horasTrabalhadas, mes, semana):
-        self.pid = str(uuid.uuid4())
-        self.id = id
-        self.horasTrabalhadas = horasTrabalhadas
-        self.mes = mes
-        self.semana = semana
-        self.date = datetime.now()
-
-
-class Vendas(db.Model):
-    vid = db.Column(db.String(255), primary_key=True, unique=True, nullable=False)
-    id = db.Column(db.String(255), db.ForeignKey("empregado.id", ondelete='CASCADE'))
-    valor = db.Column(db.Float)
-    date = db.Column(db.DateTime, nullable=False)
-    mes = db.Column(db.Integer, nullable=False)
-    semana = db.Column(db.Integer, nullable=False)
-    dia = db.Column(db.Integer, nullable=False)
-
-    def __init__(self, id, valor, mes, semana, dia):
-        self.vid = str(uuid.uuid4())
-        self.id = id
-        self.valor = valor
-        self.date = datetime.now()
-        self.mes = mes
-        self.semana = semana
-        self.dia = dia
-
-
-class Sindicato(db.Model):
-    sid = db.Column(db.String(255), primary_key=True, unique=True, nullable=False)
-    id = db.Column(db.String(255), db.ForeignKey("empregado.id", ondelete='CASCADE'))
-    membro = db.Column(db.String(255), nullable=False)
-    taxa = db.Column(db.Float)
-    taxa_add = db.Column(db.Float)
-
-    def __init__(self, id, taxa, taxa_add, membro):
-        self.sid = str(uuid.uuid4())
-        self.id = id
-        self.taxa = taxa
-        self.taxa_add = taxa_add
-        self.membro = membro
-
-
 # Creating the Routes
-
 @app.route("/ADD_EMPLOYEE", methods=["GET", "POST"])
 def add_employee():
     content = request.get_json()
@@ -180,9 +23,9 @@ def add_employee():
     db.session.add(new_employee)
 
     last_changes.append(Change(prev_data=None,
-                               new_data=new_employee,
-                               change_function="add_employee",
-                               undo_function="rmv_employee"))
+                                         new_data=new_employee,
+                                         change_function="add_employee",
+                                         undo_function="rmv_employee"))
 
     newSind = Sindicato(
         id=new_employee.id,
@@ -365,7 +208,9 @@ def Run():
     for i in horistas:
         # print("id: ", end="")
         # print(i.id)
-        horas = db.session.query(Pontos).filter(Pontos.id == str(i.id), Pontos.mes == content["mes"]).all()
+        horas = db.session.query(Pontos).filter(Pontos.id == str(i.id),
+                                                Pontos.mes == content["mes"],
+                                                Pontos.semana == content["semana"]).all()
         # print(horas)
         horas_trab = 0
 
